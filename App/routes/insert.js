0847d9a7eb596357e2fae97d99a551d3ba365730
rabhijit <40291462+rabhijit@users.noreply.github.com>
2020-04-07 21:30:41 +0800

    minor changes, moving files around

--- App/routes/insert.js
+++ App/routes/insert.js
@@ -1,23 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+// connecting to Postgres DB
 const { Pool } = require('pg')
+/* using dotenv below
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'project',
-  password: 'password',
+  database: 'postgres',
+  password: '********',
   port: 5432,
 })
+*/
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
 
 /* SQL Query */
 var sql_query = 'INSERT INTO student_info VALUES';
 
+// rendering insert.ejs page
 // GET
 router.get('/', function(req, res, next) {
 	res.render('insert', { title: 'Modifying Database' });
 });
 
+// retrieving information from insert.ejs page
 // POST
 router.post('/', function(req, res, next) {
 	// Retrieve Information
