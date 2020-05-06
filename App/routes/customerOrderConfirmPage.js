4bd834db4bc6d2be6bd8530167f704885df2c478
rabhijit <40291462+rabhijit@users.noreply.github.com>
2020-05-06 11:32:13 +0800

    started work on orderConfirmPage

--- App/routes/customerOrderConfirmPage.js
+++ App/routes/customerOrderConfirmPage.js
@@ -11,11 +11,15 @@ const pool = new Pool({
 });
 
 var customerId = null;
+var orderId = null;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	console.log(req.query.order);
-  res.render('customerOrderConfirmPage', { title: 'Express' });
+	var get_picks = "WITH Temptable AS (SELECT O.orderId, O.foodSubTotal, R.restaurantId, R.name AS rname, P.itemId, FMI.name AS iname, FMI.price, P.qtyOrdered, (P.qtyOrdered * FMI.price) AS sumPrice FROM Picks P NATURAL JOIN Orders O JOIN FoodMenuItems FMI ON (P.itemId = FMI.itemId) JOIN Restaurants R ON (FMI.restaurantId = R.restaurantId) WHERE O.customerId = " + req.query.user + " AND O.status = 'CART' ORDER BY O.orderId desc) SELECT * FROM Temptable T WHERE T.orderId = (SELECT MAX(orderId) FROM Temptable);";
+ 	pool.query(get_picks, (err, picksData) => {
+ 		console.log(picksData);
+ 		res.render('customerOrderConfirmPage', {picksData: picksData.rows});
+ 	})
 });
 
 module.exports = router;
