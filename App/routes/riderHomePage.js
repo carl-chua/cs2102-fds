b7307d15bc9614ec63e5bab23892d9f13f1c98e9
rabhijit <40291462+rabhijit@users.noreply.github.com>
2020-05-06 19:45:55 +0800

    added trigger to handle changing of restaurant in order

--- App/routes/riderHomePage.js
+++ App/routes/riderHomePage.js
@@ -14,7 +14,12 @@ router.get('/', function(req, res, next) {
 	riderId = req.query.user;
 	var riderInfoQuery = "SELECT * FROM DeliveryRiders DR WHERE riderId = " + req.query.user + " AND isDeleted = FALSE;";
 	pool.query(riderInfoQuery, (err, riderData) => {
-		res.render('riderHomePage', {riderData: riderData.rows});
+		var orderQuery = "SELECT O.orderId, O.deliveryFee, R.name AS rname, R.address AS raddress, O.address as caddress, C.name as cname, C.phoneNo, O.timePlaced FROM Orders O JOIN Restaurants R ON (O.restaurantId = R.restaurantId) JOIN Customers C ON (O.customerId = C.customerId) WHERE O.riderId = " + req.query.user + " AND O.status <> 'DELIVERED';";
+		pool.query(orderQuery, (err, orderData) => {
+			console.log(err); console.log(orderData);
+			res.render('riderHomePage', {riderData: riderData.rows, orderData: orderData.rows});			
+		})
+
 	});
 });
 
