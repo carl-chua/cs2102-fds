b180739deb52aa4d4b68ade9fd0fa4a5136c0a8b
carl-chua <carlchua0701@gmail.com>
2020-05-06 17:48:33 +0800

    orders page

--- App/routes/orders.js
+++ App/routes/orders.js
@@ -9,17 +9,27 @@ const pool = new Pool({
 
 /* SQL Query */
 //SQl only uses single quotes
-var sql_query1 =
+/*Need to select only current restaurant*/
+var pendingOrdersQuery =
   "SELECT o.orderid, o.foodsubtotal, o.timeplaced, f.name, f.price, p.qtyordered FROM ORDERS o JOIN PICKS p ON o.orderid = p.orderid JOIN FOODMENUITEMS f ON p.itemid = f.itemid WHERE o.status = 'PENDING' ORDER BY o.timeplaced";
-var sql_query3 =
+var preparingOrdersQuery =
   "SELECT o.orderid, o.foodsubtotal, o.timeplaced, f.name, f.price, p.qtyordered FROM ORDERS o JOIN PICKS p ON o.orderid = p.orderid JOIN FOODMENUITEMS f ON p.itemid = f.itemid WHERE o.status = 'PREPARING' ORDER BY o.timeplaced";
-var sql_query2 =
+var completedOrdersQuery =
   "SELECT o.orderid, o.foodsubtotal, o.timeplaced, f.name, f.price, p.qtyordered FROM ORDERS o JOIN PICKS p ON o.orderid = p.orderid JOIN FOODMENUITEMS f ON p.itemid = f.itemid WHERE o.status = 'READY-FOR-DELIVERY' OR o.status = 'DELIVERING' OR o.status = 'DELIVERED' ORDER BY o.timeplaced";
+var earningsTodayQuery = 
+  "SELECT SUM(o.foodsubtotal) as earnings FROM ORDERS o JOIN PICKS p ON o.orderid = p.orderid GROUP BY o.orderid";
 
 //This will render orderPage.ejs and return other arguments (data)
 router.get("/", function (req, res, next) {
-  pool.query(sql_query2, (err, data) => {
-    res.render("ordersPage", { title: "Orders Page", data: data.rows });
+  pool.query(pendingOrdersQuery, (err, pendingData) => {
+    pool.query(preparingOrdersQuery, (err, preparingData) => {
+      pool.query(completedOrdersQuery, (err, completedData) => {
+        pool.query(earningsTodayQuery, (err, earningsTodayData) => {
+        console.log(err);
+        res.render("ordersPage", { title: "Orders Page", pending: pendingData.rows, preparing: preparingData.rows, completed: completedData.rows, earningsToday: earningsTodayData.rows});
+        })
+      })
+    })
   });
 });
 
