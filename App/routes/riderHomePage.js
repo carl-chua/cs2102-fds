b3c87fcd2ca65071cdf7915053ec00ba3e0c0f12
rabhijit <40291462+rabhijit@users.noreply.github.com>
2020-05-06 18:56:21 +0800

    fixed riders redirection bug in App.js

--- App/routes/riderHomePage.js
+++ App/routes/riderHomePage.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var express = require('express');
-var router = express.Router();
-
 const { Pool } = require('pg')
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
 
-/* GET users listing. */
+var riderId = null;
+var riderData = null;
+
 router.get('/', function(req, res, next) {
-  res.render('customerHomePage', { title: 'Express' });
+	riderId = req.query.user;
+	var riderInfoQuery = "SELECT * FROM DeliveryRiders DR WHERE riderId = " + req.query.user + " AND isDeleted = FALSE;";
+	pool.query(riderInfoQuery, (err, riderData) => {
+		res.render('riderHomePage', {riderData: riderData.rows});
+	});
 });
 
 module.exports = router;
