fdd7a695d8e07b0dd98da7135ac140d53ef5129f
rabhijit <40291462+rabhijit@users.noreply.github.com>
2020-05-07 13:24:45 +0800

    schedule checks for MWS and WWS implemented

--- App/routes/newSchedulesPage.js
+++ App/routes/newSchedulesPage.js
@@ -29,7 +29,7 @@ function WWSconstructor(shiftArray) {
 			}
 		}
 	}
-	return shiftArray
+	return shiftArray;
 }
 
 function PGArrayConstructor(shiftArray) {
@@ -51,6 +51,58 @@ function PGArrayConstructor(shiftArray) {
 	return finalArray;
 }
 
+function MWScheck(monShift, tueShift, wedShift, thuShift, friShift, satShift, sunShift) {
+	// 5 consecutive work days in a week.
+	var workStreak = 0;
+	var highestStreak = 0;
+	var shiftArray = [monShift, tueShift, wedShift, thuShift, friShift, satShift, sunShift];
+	console.log(shiftArray);
+	for (var i = 0; i < 13; i++) {
+		if (shiftArray[i%7] != "null") {
+			workStreak++;
+		}
+		else {
+			if (workStreak > highestStreak) {
+				highestStreak = workStreak;
+			}
+			workStreak = 0;
+		}
+	}
+	if (highestStreak == 5) {
+		return true;
+	}
+	return false;
+}
+
+function WWScheck(shiftArray) {
+	// work intervals cannot exceed 4 hours
+	// must work BETWEEN 10 and 48 hours a week
+	workStreak = 0;
+	highestStreak = 0;
+	hourSum = 0;
+	for (var i = 0; i < 7; i++) {
+		for (var j = 0; j < 12; j++) {
+			if (shiftArray[i][j] == true) {
+				workStreak++;
+				hourSum++;
+			}
+			else {
+				if (workStreak > highestStreak) {
+					highestStreak = workStreak;
+					workStreak = 0;
+				}
+			}
+		}
+	}
+	if (highestStreak > 4) {
+		return false;
+	}
+	if (hourSum < 10 || hourSum > 48) {
+		return false;
+	}
+	return true;
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	riderId = req.session.riderId;
@@ -66,42 +118,52 @@ router.post('/', function(req, res, next) {
 		console.log(err); console.log(scheduleIdData);
 		var newScheduleId = scheduleIdData.rows[0].max + 1;
 		if (scheduleType == "mws") {
-			var insertMWSQuery = "INSERT INTO MonthlyWorkSchedules VALUES (" + newScheduleId + "," + req.body.mondaymws + "," + req.body.tuesdaymws + "," + req.body.wednesdaymws + "," + req.body.thursdaymws + "," + req.body.fridaymws + "," + req.body.saturdaymws + "," + req.body.sundaymws + ");";
-			console.log(req.body.startdate, req.body.enddate);
-			var insertScheduleQuery = "INSERT INTO Schedules VALUES (" + newScheduleId + "," + riderId + ",'" + req.body.startdate + "','" + req.body.enddate + "'," + "null" + "," + "'MONTHLY'" + "," + "2" + "," + "0" + "," + "1500);";
-			pool.query(insertScheduleQuery, (err, insertData) => {
-				console.log(err); console.log(insertData);
-				pool.query(insertMWSQuery, (err2, MWSData) => {
-					console.log(err2);
-					if (typeof err2 != "undefined") {
-						res.send(err.message);
-					}
-					else {
-						res.redirect("/viewSchedulesPage");
-					}
+			if (MWScheck(req.body.mondaymws, req.body.tuesdaymws, req.body.wednesdaymws, req.body.thursdaymws, req.body.fridaymws, req.body.saturdaymws, req.body.sundaymws)) {
+				var insertMWSQuery = "INSERT INTO MonthlyWorkSchedules VALUES (" + newScheduleId + "," + req.body.mondaymws + "," + req.body.tuesdaymws + "," + req.body.wednesdaymws + "," + req.body.thursdaymws + "," + req.body.fridaymws + "," + req.body.saturdaymws + "," + req.body.sundaymws + ");";
+				console.log(req.body.startdate, req.body.enddate);
+				var insertScheduleQuery = "INSERT INTO Schedules VALUES (" + newScheduleId + "," + riderId + ",'" + req.body.startdate + "','" + req.body.enddate + "'," + "null" + "," + "'MONTHLY'" + "," + "2" + "," + "0" + "," + "1500);";
+				pool.query(insertScheduleQuery, (err, insertData) => {
+					console.log(err); console.log(insertData);
+					pool.query(insertMWSQuery, (err2, MWSData) => {
+						console.log(err2);
+						if (typeof err2 != "undefined") {
+							res.send(err.message);
+						}
+						else {
+							res.redirect("/viewSchedulesPage");
+						}
+					});
 				});
-			});
+			}
+			else {
+				res.send("Please choose a schedule with 5 consecutive work days.");
+			}
 		}
 		else if (scheduleType == "wws") {
 			console.log(req.body);
 			var tempArray = [[req.body.mon10, req.body.mon11, req.body.mon12, req.body.mon13, req.body.mon14, req.body.mon15, req.body.mon16, req.body.mon17, req.body.mon18, req.body.mon19, req.body.mon20, req.body.mon21], [req.body.tue10, req.body.tue11, req.body.tue12, req.body.tue13, req.body.tue14, req.body.tue15, req.body.tue16, req.body.tue17, req.body.tue18, req.body.tue19, req.body.tue20, req.body.tue21], [req.body.wed10, req.body.wed11, req.body.wed12, req.body.wed13, req.body.wed14, req.body.wed15, req.body.wed16, req.body.wed17, req.body.wed18, req.body.wed19, req.body.wed20, req.body.wed21], [req.body.thu10, req.body.thu11, req.body.thu12, req.body.thu13, req.body.thu14, req.body.thu15, req.body.thu16, req.body.thu17, req.body.thu18, req.body.thu19, req.body.thu20, req.body.thu21], [req.body.fri10, req.body.fri11, req.body.fri12, req.body.fri13, req.body.fri14, req.body.fri15, req.body.fri16, req.body.fri17, req.body.fri18, req.body.fri19, req.body.fri20, req.body.fri21], [req.body.sat10, req.body.sat11, req.body.sat12, req.body.sat13, req.body.sat14, req.body.sat15, req.body.sat16, req.body.sat17, req.body.sat18, req.body.sat19, req.body.sat20, req.body.sat21], [req.body.sun10, req.body.sun11, req.body.sun12, req.body.sun13, req.body.sun14, req.body.sun15, req.body.sun16, req.body.sun17, req.body.sun18, req.body.sun19, req.body.sun20, req.body.sun21]];
 			var shiftArray = WWSconstructor(tempArray);
-			var stringArray = PGArrayConstructor(shiftArray);
-			console.log(stringArray);
-			var insertWWSQuery = "INSERT INTO WeeklyWorkSchedules VALUES (" + newScheduleId + "," + stringArray + ");";
-			var insertScheduleQuery = "INSERT INTO Schedules VALUES (" + newScheduleId + "," + riderId + ",'" + req.body.startdate + "','" + req.body.enddate + "'," + "null" + "," + "'WEEKLY'" + "," + "2" + "," + "0" + "," + "1500);";
-			pool.query(insertScheduleQuery, (err, insertData) => {
-				console.log(err);
-				pool.query(insertWWSQuery, (err2, WWSData) => {
-					console.log(err2);
-					if (typeof err2 != "undefined") {
-						res.send(err.message);
-					}
-					else {
-						res.redirect("/viewSchedulesPage");
-					}
+			if (WWScheck(shiftArray)) {
+				var stringArray = PGArrayConstructor(shiftArray);
+				console.log(stringArray);
+				var insertWWSQuery = "INSERT INTO WeeklyWorkSchedules VALUES (" + newScheduleId + "," + stringArray + ");";
+				var insertScheduleQuery = "INSERT INTO Schedules VALUES (" + newScheduleId + "," + riderId + ",'" + req.body.startdate + "','" + req.body.enddate + "'," + "null" + "," + "'WEEKLY'" + "," + "2" + "," + "0" + "," + "1500);";
+				pool.query(insertScheduleQuery, (err, insertData) => {
+					console.log(err);
+					pool.query(insertWWSQuery, (err2, WWSData) => {
+						console.log(err2);
+						if (typeof err2 != "undefined") {
+							res.send(err.message);
+						}
+						else {
+							res.redirect("/viewSchedulesPage");
+						}
+					})
 				})
-			})
+			}
+			else {
+				res.send("Please ensure your schedule puts in between 10 and 48 hours of work, and that your longest work interval does not exceed 4 hours.");
+			}
 		}
 	})
 });
